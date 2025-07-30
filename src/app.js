58aadb346fcd0b3267b1462001a7a40e177f421c
iamtien-cmd <lienhuetien01@gmail.com>
2025-07-30 22:35:33 +0700

    test ngay28t7

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ app.use(
 // CORS
 app.use(
     cors({
-        origin: "*" || config.CLIENT_URL || 'http://localhost:3000',
+        origin:  config.CLIENT_URL || 'http://localhost:3000',
         credentials: true,
         methods: ['GET', 'POST','PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
         allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
